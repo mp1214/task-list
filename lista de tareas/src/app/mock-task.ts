26bf199f7eb921d2941345e427885f052e657e56
mp1214 <madepato3@gmail.com>
2022-09-04 17:41:29 -0300

    ADD: mosck de tarea y su interface

---
+++ lista de tareas/src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./task"
+export const TASKS:Task[] =[
+
+{
+    id: 1,
+    text: "terminar primer modulo de angular",
+    day: "agosto 5 de 2022",
+    reminder: true,
+},
+{
+    id: 2,
+    text: "empezar segundo modulo de angular",
+    day: "agosto 19 de 2022",
+    reminder: false,
+},
+{
+    id: 3,
+    text: "preparar examen aws particioner",
+    day: "agosto 21 de 2022",
+    reminder: false,
+},
+{
+    id: 4,
+    text: "Hacer tp de codo a codo",
+    day: "agosto 28 de 2022",
+    reminder: true,
+}
+]
