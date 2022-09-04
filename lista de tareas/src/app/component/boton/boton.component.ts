9957a0b02dfbad335d678729edd5bc9a3f489d9b
mp1214 <madepato3@gmail.com>
2022-09-04 17:20:42 -0300

    Primer boton

---
+++ lista de tareas/src/app/component/boton/boton.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-boton',
+  templateUrl: './boton.component.html',
+  styleUrls: ['./boton.component.css']
+})
+export class BotonComponent implements OnInit {
+@Input() text: string ="";
+@Input() color: string="";
+@Output() bynClick =new EventEmitter;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onClick(){
+   this.bynClick.emit();
+  }
+}
